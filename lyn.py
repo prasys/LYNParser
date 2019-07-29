d9fb7cb7c857031ef17d4a7d87d5b4e543ca8dd6
Pradeesh <pradeesh@prasys.info>
2019-07-29 14:54:18 +1200

    Update lyn.py
    Included the function to fetch for each topic. Will need to optimize and remove the noise

--- lyn.py
+++ lyn.py
@@ -6,14 +6,16 @@ import datetime
 import urllib3
 import re
 
-
+url = 'https://forum.lowyat.net/Kopitiam'
+LYNTOPICURL = 'https://forum.lowyat.net'
 def make_soup(url):
     http = urllib3.PoolManager()
     r = http.request("GET", url)
     return BeautifulSoup(r.data,'lxml')
 
 
-## This will parse the bloody LYN
+## This will parse the LYN Topic Parser
+## It should extract the topic
 def parseLYN(url):
     
     result = []
@@ -24,7 +26,10 @@ def parseLYN(url):
     divs = soup.find_all("div",{'style':'float:left'})
    # print(divs)
     for div in divs:
-        print div.find('a').contents[0] ## Print's the Topic 
+        print div.find('a').contents[0] ## Print's the Topic
+        print div.find('a')['href']
+        parseThread(div.find('a')['href'])
+        break;
         ##print(div.text)
         ###break
         ##result = re.match(".*?(?=\s{2})",div.text)
@@ -37,6 +42,16 @@ def parseLYN(url):
        
 
         
+
+def parseThread(topicID):
+
+    result = []
+    req = requests.get(LYNTOPICURL + topicID)
+    soup = BeautifulSoup(req.text, "lxml")
+    # postcolor post_text - LYN uses it to tell if it is a post or not. We will take a look at it 
+    thread = soup.find_all("div",{'class':'postcolor post_text'})
+    for post in thread:
+        print post.text
 
 
  
